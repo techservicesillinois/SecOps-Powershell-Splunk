329426f155ec99144aa7c82e5edad3471720f747
Mark Baumgartner <42626826+mabaumgartner@users.noreply.github.com>
2022-09-06 10:45:03 -0500

    remove trailing space

--- src/UofISplunkCloud/functions/public/Send-SplunkHECEvent.ps1
+++ src/UofISplunkCloud/functions/public/Send-SplunkHECEvent.ps1
@@ -80,7 +80,7 @@ function Send-SplunkHECEvent {
 
             # ConvertTo-Json escapes unicode U+0022 quotes automatically
             # https://www.ietf.org/rfc/rfc8259.txt
-            # Splunk HEC seems to interpret other unicode quotes as legitimate quotes 
+            # Splunk HEC seems to interpret other unicode quotes as legitimate quotes
             # Escape these quotes to prevent a HEC error of: text":"Invalid data format","code":6,"
             $Body = $Body -replace "`u{201c}", "\`u{201c}" -replace "`u{201d}", "\`u{201d}" -replace "`u{201f}", "\`u{201f}"
 
